944574436667037f84310348de114a80fab6c765
seansio1995 <seansio@163.com>
2016-04-28 17:49:06 +0800

    done

--- countFingers/ViewController.swift
+++ countFingers/ViewController.swift
@@ -10,6 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var guessNum: UITextField!
+    
+    @IBOutlet weak var result: UILabel!
+    
+    @IBAction func guess(sender: AnyObject) {
+        var randomNum=arc4random_uniform(6)
+        
+        if guessNum.text.toInt() != nil {
+    
+        if Int(randomNum) == guessNum.text.toInt(){
+            result.text="You guess correctly"
+        }
+        
+        else
+        {
+            result.text="Wrong! The correct is \(randomNum)"
+        }
+    }
+        else
+        {
+            result.text="Please enter a number"
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
